9dbf44c13afa98effeb113c99c1181c36651ace1
chaoren1641 <chaoren1641@gmail.com>
2013-08-28 18:53:13 +0800

    详情页右上角登录逻辑

--- app/iconfont/lte-ie7.js
+++ app/iconfont/lte-ie7.js
@@ -1,99 +1,101 @@
 /* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
 
 window.onload = function() {
-	function addIcon(el, entity) {
-		var html = el.innerHTML;
-		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
-	}
-	var icons = {
-			'icon-yundun' : '&#x22;',
-			'icon-yes3' : '&#x23;',
-			'icon-yes2' : '&#x24;',
-			'icon-yes' : '&#x25;',
-			'icon-wrong' : '&#x26;',
-			'icon-video' : '&#x27;',
-			'icon-upload' : '&#x28;',
-			'icon-up2' : '&#x29;',
-			'icon-up' : '&#x2a;',
-			'icon-unlock' : '&#x2b;',
-			'icon-strap' : '&#x2d;',
-			'icon-stop' : '&#x2e;',
-			'icon-star2' : '&#x2f;',
-			'icon-star' : '&#x30;',
-			'icon-slb' : '&#x31;',
-			'icon-shopping-cart' : '&#x32;',
-			'icon-set' : '&#x34;',
-			'icon-Search' : '&#x35;',
-			'icon-sale' : '&#x36;',
-			'icon-rubbish' : '&#x37;',
-			'icon-right2' : '&#x38;',
-			'icon-right' : '&#x39;',
-			'icon-reload' : '&#x3a;',
-			'icon-rds' : '&#x3b;',
-			'icon-play' : '&#x3c;',
-			'icon-pic' : '&#x3d;',
-			'icon-phone' : '&#x3e;',
-			'icon-pen' : '&#x3f;',
-			'icon-ots' : '&#x40;',
-			'icon-oss' : '&#x41;',
-			'icon-order' : '&#x42;',
-			'icon-odps' : '&#x43;',
-			'icon-no3' : '&#x44;',
-			'icon-mail' : '&#x45;',
-			'icon-lock' : '&#x46;',
-			'icon-loading' : '&#x47;',
-			'icon-link' : '&#x48;',
-			'icon-lessen' : '&#x49;',
-			'icon-left2' : '&#x4a;',
-			'icon-left' : '&#x4b;',
-			'icon-untitled' : '&#x4c;',
-			'icon-home' : '&#x4d;',
-			'icon-heart2' : '&#x4e;',
-			'icon-heart' : '&#x4f;',
-			'icon-good' : '&#x50;',
-			'icon-giving' : '&#x51;',
-			'icon-file' : '&#x52;',
-			'icon-ecs' : '&#x53;',
-			'icon-earth' : '&#x54;',
-			'icon-download' : '&#x55;',
-			'icon-down2' : '&#x56;',
-			'icon-down' : '&#x57;',
-			'icon-customers' : '&#x5b;',
-			'icon-customer' : '&#x5c;',
-			'icon-counter' : '&#x5d;',
-			'icon-cloud' : '&#x5e;',
-			'icon-clock' : '&#x5f;',
-			'icon-card' : '&#x60;',
-			'icon-calendar' : '&#x61;',
-			'icon-blow-up' : '&#x62;',
-			'icon-appendix' : '&#x63;',
-			'icon-ace' : '&#x64;',
-			'icon-reduce' : '&#x67;',
-			'icon-bang2' : '&#x68;',
-			'icon-bang' : '&#x69;',
-			'icon-add' : '&#x6a;',
-			'icon-yunjiankong' : '&#x21;',
-			'icon-cdn' : '&#x33;',
-			'icon-tel' : '&#x59;',
-			'icon-help' : '&#x5a;',
-			'icon-dialog2' : '&#x65;',
-			'icon-down3' : '&#x2c;'
-		},
-		els = document.getElementsByTagName('*'),
-		i, attr, html, c, el;
-	for (i = 0; ; i += 1) {
-		el = els[i];
-		if(!el) {
-			break;
-		}
-		attr = el.getAttribute('data-icon');
-		if (attr) {
-			addIcon(el, attr);
-		}
-		c = el.className;
-		c = c.match(/icon-[^\s'"]+/);
-		if (c && icons[c[0]]) {
-			addIcon(el, icons[c[0]]);
-		}
-	}
+  function addIcon(el, entity) {
+    var html = el.innerHTML;
+    el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+  }
+  var icons = {
+      'icon-oss' : '&#x21;',
+      'icon-yunjiankong' : '&#x22;',
+      'icon-yundun' : '&#x23;',
+      'icon-yes3' : '&#x24;',
+      'icon-yes2' : '&#x25;',
+      'icon-wrong' : '&#x26;',
+      'icon-video' : '&#x27;',
+      'icon-upload' : '&#x28;',
+      'icon-up2' : '&#x29;',
+      'icon-up' : '&#x2a;',
+      'icon-unlock' : '&#x2b;',
+      'icon-tel' : '&#x2c;',
+      'icon-strap' : '&#x2d;',
+      'icon-stop' : '&#x2e;',
+      'icon-star2' : '&#x2f;',
+      'icon-star' : '&#x30;',
+      'icon-slb' : '&#x31;',
+      'icon-shopping-cart' : '&#x32;',
+      'icon-set2' : '&#x33;',
+      'icon-set' : '&#x34;',
+      'icon-Search' : '&#x35;',
+      'icon-sale' : '&#x36;',
+      'icon-rubbish' : '&#x37;',
+      'icon-right2' : '&#x38;',
+      'icon-right' : '&#x39;',
+      'icon-reload' : '&#x3a;',
+      'icon-reduce' : '&#x3b;',
+      'icon-rds' : '&#x3c;',
+      'icon-play' : '&#x3d;',
+      'icon-pic' : '&#x3e;',
+      'icon-phone' : '&#x3f;',
+      'icon-pen' : '&#x40;',
+      'icon-ots' : '&#x41;',
+      'icon-order' : '&#x42;',
+      'icon-odps' : '&#x43;',
+      'icon-ocs' : '&#x44;',
+      'icon-no3' : '&#x45;',
+      'icon-money' : '&#x46;',
+      'icon-mail' : '&#x47;',
+      'icon-lock' : '&#x48;',
+      'icon-loading' : '&#x49;',
+      'icon-link' : '&#x4a;',
+      'icon-lessen' : '&#x4b;',
+      'icon-left2' : '&#x4c;',
+      'icon-left' : '&#x4d;',
+      'icon-home' : '&#x4e;',
+      'icon-help' : '&#x4f;',
+      'icon-heart2' : '&#x50;',
+      'icon-heart' : '&#x51;',
+      'icon-good' : '&#x52;',
+      'icon-giving' : '&#x53;',
+      'icon-file' : '&#x54;',
+      'icon-ecs' : '&#x55;',
+      'icon-earth' : '&#x56;',
+      'icon-download' : '&#x57;',
+      'icon-down3' : '&#x58;',
+      'icon-down2' : '&#x59;',
+      'icon-down' : '&#x5a;',
+      'icon-dialog2' : '&#x5b;',
+      'icon-customers' : '&#x5c;',
+      'icon-customer' : '&#x5d;',
+      'icon-counter' : '&#x5e;',
+      'icon-cloud' : '&#x5f;',
+      'icon-clock' : '&#x60;',
+      'icon-card' : '&#x61;',
+      'icon-calendar' : '&#x62;',
+      'icon-blow-up' : '&#x63;',
+      'icon-bang2' : '&#x64;',
+      'icon-bang' : '&#x65;',
+      'icon-appendix' : '&#x66;',
+      'icon-add' : '&#x67;',
+      'icon-ace' : '&#x68;',
+      'icon-info' : '&#x69;',
+      'icon-cdn' : '&#x6a;'
+    },
+    els = document.getElementsByTagName('*'),
+    i, attr, html, c, el;
+  for (i = 0; ; i += 1) {
+    el = els[i];
+    if(!el) {
+      break;
+    }
+    attr = el.getAttribute('data-icon');
+    if (attr) {
+      addIcon(el, attr);
+    }
+    c = el.className;
+    c = c.match(/icon-[^\s'"]+/);
+    if (c && icons[c[0]]) {
+      addIcon(el, icons[c[0]]);
+    }
+  }
 };
